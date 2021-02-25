d4bfef93c36fee6f73bae1aaf791d7eb6ccf515f
orange3710 <78603129+orange3710@users.noreply.github.com>
2021-02-25 23:16:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -17,5 +17,25 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+# 処理
 
+@bot.event
+async def on_message(message):
+    # 処理
+    await bot.process_commands(message)
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member : discord.Member, reason=None):
+    kicknoti = discord.Embed(title='メンバーをキックしました', description='Kickしたメンバーにまた来てもらうには再招待してください', color=discord.Color.red())
+    kicknoti.add_field(name='執行人', value=f'{ctx.author.mention}')
+    kicknoti.add_field(name='Kickされた人', value=f'{member.mention}')
+    kicknoti.set_thumbnail(url=member.avatar_url)
+    await ctx.send(embed=kicknoti)
+    await member.kick(reason=reason)
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
+bot.run('ODE0MDkwMzI5ODk5NjYzMzkw.YDYysw.H4xEwpQ1Hbvy0IMw1lgE0dF1Qpo')
 bot.run(token)
